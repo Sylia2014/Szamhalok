14d36d0de7b06f05b3b8e98296e2dce36ab0fe03
Sylia <thalyzsozsi@gmail.com>
2019-10-07 06:32:29 +0200

    project update

--- client.py
+++ client.py
@@ -7,55 +7,61 @@ with open(sys.argv[1], "r") as file:
 
 endPoints = data['end-points']
 switches = data['switches']
-links = data['links']
 possibleCircuits = data['possible-circuits']
 simulation = data['simulation']
-demands = simulation['demands']
 duration = simulation['duration']
+
+links: list
+links = data['links']
+
+demands: list
+demands = simulation['demands']
+
+link: dict
+for link in links:
+    link.update({'used-capacity': 0})
+
+demand: dict
+for demand in demands:
+    demand.update({'started': False})
+    demand.update({'connection': None})
 
 connections = []
 usedLinks = []
 availableCircuits = possibleCircuits
-availableLinks = links
 
 
 def validLink(point1, point2):
     return (point1[0] == point2[0] and point1[-1] == point2[1]) or (point1[0] == point2[1] and point1[-1] == point2[0])
 
-def possibleCircuit(circuits, endpoints, demand): #ezzel megnézzük van-e egyáltalán olyan áramkör, aminek a két vége a nekünk kellő két pont
+def checkIsPossibleCircuit(circuits, endpoints, demand): #ezzel megnézzük van-e egyáltalán olyan áramkör, aminek a két vége a nekünk kellő két pont
     needToTest, result = [], []
     for circuit in circuits:
-        if circuit in availableCircuits and validLink(circuit, endpoints):
-            needToTest.append(circuit)
-    if needToTest:
-        result = useableConnection(needToTest, demand)
+        if validLink(circuit, endpoints):
+            isUseableCircuit(circuit, demand)
+
     return result
 
-def useableConnection(possConns, demand): #ezzel megnézzük hogy a lehetséges áramkörök közül tudjuk-e bármelyiket ténylegesen használni
-    for conn in possConns:
-        useableRoute = True
-        maybeDelLinks = []
-        for i in range(len(conn)-1):
-            useableLink = False
-            arr = [conn[i], conn[i+1]]
-            for j in range(len(availableLinks)):
-                actualLinks = availableLinks[j]
-                if validLink(arr, actualLinks['points']) and demand == actualLinks['capacity']:
-                    maybeDelLinks.append(arr)
-                    useableLink |= True
-                    useableRoute &= True
-                    # deleteFromConnections(availableLinks, arr)
-                    break
-                if i == len(conn)-1 and not(validLink(arr, actualLinks['points']) and demand == actualLinks['capacity']):
-                    useableRoute &= False
-                # else:
-                #     useable = False
-                #     break
-        if useableRoute:
-            deleteFromLinks(availableLinks, maybeDelLinks)
-            print(availableLinks)
-            return conn
-    return []
+def isUseableCircuit(circuit, demand): #ezzel megnézzük hogy a lehetséges áramkörök közül tudjuk-e bármelyiket ténylegesen használni
+    for i in range(len(circuit) - 1):
+        for j in range(len(links)):
+            actualLinks = links[j]
+            if validLink(circuit[i:i+2], actualLinks['points']) and demand <= actualLinks['capacity'] - actualLinks['used-capacity']:
+                break
+            # if (i == len(circuit) - 1 and j == range(len(links))
+            #         and not(validLink(circuit[i:i+2], actualLinks['points']) and demand <= actualLinks['capacity'] - actualLinks["used-capacity"])):
+            #     return False
+
+
+    # for conn in possConns:
+    #     useableRoute = True
+    #     maybeDelLinks = []
+    #
+    #     if useableRoute:
+    #         deleteFromLinks(availableLinks, maybeDelLinks)
+    #         print(availableLinks)
+    #         return conn
+    # return []
 
 
 def deleteFromConnections(connections, connection):
@@ -80,20 +86,22 @@ def backToAvaliableLinks(availableLinks, connection):
 action = 1
 for i in range(duration):
     for elem in demands:
-        connection = possibleCircuit(availableCircuits, elem['end-points'], elem['demand'])
-        if elem['start-time'] == i and connection:
-            connections.append(connection)
-            deleteFromConnections(availableCircuits, connection)
+        # connection = possibleCircuit(availableCircuits, elem['end-points'], elem['demand'])
+        if elem['start-time'] == i:
+            checkIsPossibleCircuit(possibleCircuits, elem['end-points'], elem['demand'])
+            # connections.append(connection)
+            # deleteFromConnections(availableCircuits, connection)
             print(str(action) + ". igény foglalás: " + elem['end-points'][0] + "<->" + elem['end-points'][1] + " st:" + str(i) + " - sikeres")
             action += 1
-        elif elem['start-time'] == i and not(connection):
-            print(str(action) + ". igény foglalás: " + elem['end-points'][0] + "<->" + elem['end-points'][1] + " st:" + str(i) + " - sikertelen")
-            action += 1
+        # elif elem['start-time'] == i and not(connection):
+        #     print(str(action) + ". igény foglalás: " + elem['end-points'][0] + "<->" + elem['end-points'][1] + " st:" + str(i) + " - sikertelen")
+        #     action += 1
 
-        if elem['end-time'] == i:
-            deleteFromConnections(connections, connection)
-            availableCircuits.append(connection)
-            # vissza kell tenni a linkeket is az availableLinksbe
-            backToAvaliableLinks(availableLinks,connection)
+        if elem['end-time'] == i and elem['started']:
+            # felszabadít
+            # deleteFromConnections(connections, connection)
+            # availableCircuits.append(connection)
+            # # vissza kell tenni a linkeket is az availableLinksbe
+            # backToAvaliableLinks(availableLinks,connection)
             print(str(action) + ". igény felszabadítás: " + elem['end-points'][0] + "<->" + elem['end-points'][1] + " st:" + str(i))
             action += 1
